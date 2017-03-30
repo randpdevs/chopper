3e58c12c102b16b51d55497acb58a1df868f94aa
rakesh <rakesh@syonlab.com>
2017-03-30 12:37:34 +0530

    Added score in getRanking api

--- genQues/APIv1/getRanking.py
+++ genQues/APIv1/getRanking.py
@@ -2,10 +2,9 @@ from ..models import *
 
 def rankingFunction(data):
     rankingObj = rankingSet.objects.filter(questionA=data['questionID']).order_by('-userScore')
-    print rankingObj
     sendData = []
     rank = 0
     for i in rankingObj:
         rank = rank+1
-        sendData.append({"userName":i.userName,"Rank":rank})
+        sendData.append({"userName":i.userName,"Rank":rank,"userScore":i.userScore})
     return sendData
