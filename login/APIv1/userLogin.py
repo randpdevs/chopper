b216e6dc9a3521372c76fc68b24a952976fde0ad
rakesh <rakesh@syonlab.com>
2017-04-02 23:17:35 +0530

    API login changed

--- login/APIv1/userLogin.py
+++ login/APIv1/userLogin.py
@@ -2,8 +2,11 @@ from ..models import UserModel
 import bcrypt
 
 def userLogin(request):
-    userObject = UserModel.objects.get(UserName=request['userName'])
-    if bcrypt.checkpw(str(request['password']),str(userObject.Password)):
-        return 202
-    else:
-        return 400
+    try:
+        userObject = UserModel.objects.get(UserName=request['userName'])
+        if bcrypt.checkpw(str(request['password']),str(userObject.Password)):
+            return 202
+        else:
+            return 400
+    except:
+        return 404
