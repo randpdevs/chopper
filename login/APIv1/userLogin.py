66ed89736f96869a0abca604f044ce7020006be4
rakesh <rakesh@syonlab.com>
2017-04-03 13:37:04 +0530

    API login will return user detail

--- login/APIv1/userLogin.py
+++ login/APIv1/userLogin.py
@@ -5,7 +5,13 @@ def userLogin(request):
     try:
         userObject = UserModel.objects.get(UserName=request['userName'])
         if bcrypt.checkpw(str(request['password']),str(userObject.Password)):
-            return 202
+            returnData={}
+            returnData['UserName']=userObject.UserName
+            returnData['EmailID']=userObject.EmailID
+            returnData['Age'] = userObject.Age
+            returnData['Country'] = userObject.Country
+
+            return returnData
         else:
             return 400
     except:
