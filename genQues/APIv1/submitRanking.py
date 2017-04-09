cdde820753a68e5286c09d8afa75f4ecefef876b
rakesh <rakesh@syonlab.com>
2017-04-09 12:37:22 +0530

    added check for time travellers

--- genQues/APIv1/submitRanking.py
+++ genQues/APIv1/submitRanking.py
@@ -10,28 +10,28 @@ def submitRank(data):
     except:
         return '404'
     print currentEpoch, quesObj.questionBoutEndStamp, quesObj.questionTimeStamp
-    # if currentEpoch <= int(quesObj.questionBoutEndStamp) and currentEpoch >= int(quesObj.questionTimeStamp):
-    try:
-        rankingObj = rankingSet()
-        rankingObj.questionID = data['questionID']
-        rankingObj.userName = data['userName']
-        rankingObj.userScore = data['userScore']
+    if currentEpoch <= int(quesObj.questionBoutEndStamp) and currentEpoch >= int(quesObj.questionTimeStamp):
         try:
-            userModelObj = UserModel.objects.get(UserName = data['userName'])
-            userModelObj.UserScore = max(data['userScore'],userModelObj.UserScore)
-            userModelObj.save()
-        except :
-            pass
-        try:
-            userStatAPI.addUserStat(data,userModelObj)
-        except Exception as e:
-            pass
+            rankingObj = rankingSet()
+            rankingObj.questionID = data['questionID']
+            rankingObj.userName = data['userName']
+            rankingObj.userScore = data['userScore']
+            try:
+                userModelObj = UserModel.objects.get(UserName = data['userName'])
+                userModelObj.UserScore = max(data['userScore'],userModelObj.UserScore)
+                userModelObj.save()
+            except :
+                pass
+            try:
+                userStatAPI.addUserStat(data,userModelObj)
+            except Exception as e:
+                pass
 
-        rankingObj.correctans = data['correctans']
-        rankingObj.wrongans = data['wrongans']
-        rankingObj.save()
-        return "202-2"
-    except Exception as e:
-        return '400'
-    # else:
-    #     return '202-1'
+            rankingObj.correctans = data['correctans']
+            rankingObj.wrongans = data['wrongans']
+            rankingObj.save()
+            return "202-2"
+        except Exception as e:
+            return '400'
+    else:
+        return '202-1'
